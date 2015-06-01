d92df2a34e1315742436fbdcc6515152819ba3b2
Miika Arponen <miika@geniem.com>
2015-06-01 16:12:04 +0300

    changed name

--- frontend-login.php
+++ frontend-login.php
@@ -1,6 +1,6 @@
 <?php
 /*
-Plugin Name: Cluster login
+Plugin Name: Frontend login
 Plugin URI: http://www.geniem.com
 Description: A plugin that logs user in to multiple instances of the same site at once
 Author: Miika Arponen / Geniem Oy
@@ -8,7 +8,7 @@ Author URI: http://www.geniem.com
 Version: 0.0.1
 */
 
-class Cluster_Login {
+class Frontend_Login {
 	
 	public function __construct() {
 		add_action("init", array( $this, "check_login" ) );
@@ -23,7 +23,7 @@ class Cluster_Login {
 
 	public function check_login() {
 		if ( ! is_user_logged_in() ) {
-			if ( preg_match( "/cluster_login_token=([^&]+)&?/", $_SERVER["REQUEST_URI"], $matches ) ) {
+			if ( preg_match( "/frontend_login_token=([^&]+)&?/", $_SERVER["REQUEST_URI"], $matches ) ) {
 				$compare_token = md5($_SERVER["HTTP_USER_AGENT"] . "#ß°¶0bÄæ"); // . $_SERVER['REMOTE_ADDR']);
 
 				if ( strpos( $matches[1], $compare_token ) === false ) {
@@ -31,7 +31,7 @@ class Cluster_Login {
 				}
 
 				// find out if the token exists and get the user for it
-				$users = get_users( array( "meta_key" => "_cluster_login_token",
+				$users = get_users( array( "meta_key" => "_frontend_login_token",
 										   "meta_value" => $matches[1]
 										 ) );
 
@@ -42,7 +42,7 @@ class Cluster_Login {
 				$user = $users[0];
 
 				// get the timestamp from the database
-				$timestamp = get_user_meta( $user->ID, "_cluster_login_timestamp", true );
+				$timestamp = get_user_meta( $user->ID, "_frontend_login_timestamp", true );
 
 				// if the timestamp is not too old, log the user in
 				if ( isset( $timestamp ) && ( time() - $timestamp < 300 ) ) {
@@ -52,29 +52,29 @@ class Cluster_Login {
 				}
 
 				// delete the token and timestamp from the database to prevent misuse
-				delete_user_meta( $user->ID, "_cluster_login_token" );
-				delete_user_meta( $user->ID, "_cluster_login_timestamp" );
+				delete_user_meta( $user->ID, "_frontend_login_token" );
+				delete_user_meta( $user->ID, "_frontend_login_timestamp" );
 
 				return;
 			}
 		}
-		else if ( preg_match( "/cluster_logout/", $_SERVER["REQUEST_URI"] ) ) {
+		else if ( preg_match( "/frontend_logout/", $_SERVER["REQUEST_URI"] ) ) {
 			wp_logout();
 		}
 	}
 
 	public function admin_stuff() {
-		if ( preg_match( "/cluster_logout/", $_SERVER["REQUEST_URI"] ) ) {
+		if ( preg_match( "/frontend_logout/", $_SERVER["REQUEST_URI"] ) ) {
 			wp_logout();
 		}
-		else if ( isset( $_SESSION["cluster_login_done"] ) ) {
+		else if ( isset( $_SESSION["frontend_login_done"] ) ) {
 			return;
 		}
 		else {
 			$user_id = get_current_user_id();
 
 			// register the javascript code we need
-			wp_register_script( "cluster_login", plugin_dir_url( __FILE__ ) . "/js/cluster-login.js", array("jquery"), "0.01" );
+			wp_register_script( "frontend_login", plugin_dir_url( __FILE__ ) . "/js/frontend-login.js", array("jquery"), "0.01" );
 
 			// create the token
 			$token = md5($_SERVER["HTTP_USER_AGENT"] . "#ß°¶0bÄæ") . substr( md5(microtime()), 3, 8 ); // . $_SERVER['REMOTE_ADDR']);
@@ -85,29 +85,29 @@ class Cluster_Login {
 			$data["token"] = $token;
 
 			// pass the data to javascript
-			wp_localize_script( "cluster_login", "cluster_login_data", $data );
+			wp_localize_script( "frontend_login", "frontend_login_data", $data );
 
 			// enqueue the javascript
-			wp_enqueue_script("cluster_login");
+			wp_enqueue_script("frontend_login");
 
 			// insert the token into database
-			add_user_meta( $user_id, "_cluster_login_token", $token, true );
+			add_user_meta( $user_id, "_frontend_login_token", $token, true );
 
 			// insert timestamp into database
-			add_user_meta( $user_id, "_cluster_login_timestamp", time(), true );
+			add_user_meta( $user_id, "_frontend_login_timestamp", time(), true );
 
-			$_SESSION["cluster_login_done"] = true;
+			$_SESSION["frontend_login_done"] = true;
 		}
 	}
 
 	public function logout() {
-		unset($_SESSION["cluster_login_done"]);
+		unset($_SESSION["frontend_login_done"]);
 	}
 
 	public function custom_login() {
 		if ( preg_match( "/loggedout/", $_SERVER["REQUEST_URI"] ) ) {
 			// register the javascript code we need
-			wp_register_script( "cluster_login", plugin_dir_url( __FILE__ ) . "/js/cluster-login.js", array("jquery"), "0.01" );
+			wp_register_script( "frontend_login", plugin_dir_url( __FILE__ ) . "/js/frontend-login.js", array("jquery"), "0.01" );
 
 			// initialize the data
 			$data = array();
@@ -115,19 +115,19 @@ class Cluster_Login {
 			$nonce = wp_create_nonce( "logout" );
 
 			if ( preg_match( "/admin/", $_SERVER['SERVER_NAME'] ) ) {
-				$data["url"] = WP_PUB_SITEURL ."/?cluster_logout";	
+				$data["url"] = WP_PUB_SITEURL ."/?frontend_logout";	
 			}
 			else {
-				$data["url"] = WP_ADMIN_URL ."/?cluster_logout";
+				$data["url"] = WP_ADMIN_URL ."/?frontend_logout";
 			}
 
 			// pass the data to javascript
-			wp_localize_script( "cluster_login", "cluster_login_data", $data );
+			wp_localize_script( "frontend_login", "frontend_login_data", $data );
 
 			// enqueue the javascript
-			wp_enqueue_script("cluster_login");
+			wp_enqueue_script("frontend_login");
 		}
 	}
 }
 
-new Cluster_Login();
+new Frontend_Login();
