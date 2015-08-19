d76c08456ba5f2cda024d349c29ba5c61327b18e
Miika Arponen <miika@geniem.com>
2015-08-19 16:21:50 +0300

    works now with cluster-login plugin

--- js/frontend-login.js
+++ js/frontend-login.js
@@ -1,8 +1,8 @@
 jQuery(document).ready( function($) {
-	var url = cluster_login_data.url;
-	var token = cluster_login_data.token;
+	var url = frontend_login_data.url;
+	var token = frontend_login_data.token;
 
-	if(token) var temp_iframe = $("<iframe/>").attr("src", url + "/?cluster_login_token=" + token);
+	if(token) var temp_iframe = $("<iframe/>").attr("src", url + "/?frontend_login_token=" + token);
 	else var temp_iframe = $("<iframe/>").attr("src", url);
 
 	$(temp_iframe).appendTo("body").hide();
